822007cc04eed0fd22012d3eaa91c7eed63036bb
Manojmehra22 <manojmehr650@gmail.com>
2025-01-27 22:40:36 +0530

    React form updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState();
+  const inputEvent = (event) => {
+    event.preventDefault();
+    setName(event.target.value);
+    console.log(event.target.value);
+  };
+  const onSubmit = () => {
+    setFullName(name);
+  };
   return (
     <>
       <div>
-        <h1>Hello</h1>
-        <input type="text" placeholder="Enter your Name" />
-        <button>Click Me☺️</button>
+        <h1>Result {fullName}</h1>
+        <input
+          type="text"
+          placeholder="Enter your Name"
+          onChange={inputEvent}
+          value={name}
+        />
+        <button onClick={onSubmit}>Click Me☺️</button>
       </div>
     </>
   );
