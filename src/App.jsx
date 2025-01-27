7e52f515b263a28d69ad448e94bd7a42642e9cd3
Manojmehra22 <manojmehr650@gmail.com>
2025-01-27 17:30:59 +0530

    React form add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <input type="text" placeholder="Enter your Name" />
+        <button>Click Me☺️</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
